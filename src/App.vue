1c37c346a5d72948cb159b288eeca83002d95322
derekfan0317 <131131426+derekfan0317@users.noreply.github.com>
2024-08-11 22:14:35 +1000

    Activity4-7

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,8 @@ import LoginForm from './components/LoginForm.vue'
 </script>
 
 <template>
-  <header>
-    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
-
-    <!-- <div class="wrapper">
-        <HelloWorld msg="You did it!" />
-    </div> -->
-  </header>
-
-  <main>
-    <!-- <JSON /> -->
-    <LoginForm />
-    <!-- <TheWelcome /> -->
-  </main>
+  <!-- <JSON /> -->
+  <LoginForm />
 </template>
 
 <style scoped>
@@ -43,7 +32,6 @@ header {
   header .wrapper {
     display: flex;
     place-items: flex-start;
-    flex-wrap: wrap;
   }
 }
 </style>
