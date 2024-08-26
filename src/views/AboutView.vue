de7da5893b56a6d1801e985eb5121e9b0e285912
derekfan0317 <131131426+derekfan0317@users.noreply.github.com>
2024-08-26 21:42:47 +1000

    style consistent

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,10 +1,16 @@
 <template>
-  <div class="about">
-    <h1>About Our Library</h1>
-    <p>
-      Welcome to our digital library! We're dedicated to providing a vast collection of books and
-      resources to our community.
-    </p>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-md-8 offset-md-2">
+        <div class="about">
+          <h1 class="text-center">About Our Library</h1>
+          <p class="text-center">
+            Welcome to our digital library! We're dedicated to providing a vast collection of books
+            and resources to our community.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
